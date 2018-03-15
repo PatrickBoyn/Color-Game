34f2b953aa58e72024309f74f3928238ea6ed91c
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-03-14 23:07:38 -0400

    This puts the rgb value into string format so that it can be gotten later.

--- Colors.js
+++ Colors.js
@@ -64,6 +64,7 @@ function randomColor(){
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + "," + g  + "," + b + ")"
 };
 
 console.log(correct);
