863679fd189cbbdf8df789bd4e565c7b57e289cf
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-03-14 22:15:49 -0400

    It's either got to be square, color, colors, or correct.

--- Colors.js
+++ Colors.js
@@ -47,7 +47,7 @@ for(var i = 0; i < square.length; i++){
 // Changes all squares to the color of the correct square
 function colorChange(color){
     for(var i = 0; i < square.length; i++){
-        square[i].style.background = correct[i];
+        square[i].style.background = squares[i];
         this.backgroundColor = correct;
     }
 };
