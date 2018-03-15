c7055dace05b3e036bb8ee081a9e1e954e9a820a
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-03-14 22:16:42 -0400

    It's not color.

--- Colors.js
+++ Colors.js
@@ -47,7 +47,7 @@ for(var i = 0; i < square.length; i++){
 // Changes all squares to the color of the correct square
 function colorChange(color){
     for(var i = 0; i < square.length; i++){
-        square[i].style.background = squares[i];
+        square[i].style.background = color[i];
         this.backgroundColor = correct;
     }
 };
