c1cff6475ea9b63209901f2b22fe659c436ff40c
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-03-19 15:49:50 -0400

    Moved everything to an init function.

--- Colors.js
+++ Colors.js
@@ -16,25 +16,8 @@ var mode = document.querySelectorAll(".mode");
 // No longer hard coded, picked by a function at the bottom of the code
 var correct = colorPicker();
 
-picker.textContent = correct;
+init();
 
-for(var i = 0; i < mode.length; i++){
-    mode[i].addEventListener("click", function(){
-        // If one is selected when the other is clicked, turn it off
-        // I came up with this part on my own. 
-        if(mode[0] === "selected"){
-            select();
-        }else{
-            select();
-        }
-        
-        // Complicated if statement with a ternary operator 
-        this.textContent ==="Easy" ? numsquares = 3: numsquares = 6;
-        
-        change();
-        console.log("Mode clicked. ");
-    });
-}
 // Which mode to toggle on and off. 
 function select(){
     mode[1].classList.toggle("selected");
@@ -65,33 +48,6 @@ console.log("Mode length is: " + mode.length);
 reset.addEventListener("click", function(){
     change();
 });
-
-// Sets the span to show the correct variable's RBG value
-// picker.textContent = correct;
-// Loops through all of the squares and gives them a different color
-for(var i = 0; i < square.length; i++){
-    
-    square[i].style.backgroundColor = colors[i];
-    
-    // Makes all of the squares clickable colors
-    square[i].addEventListener("click", function(){
-        // Tests to see if squares are clickable
-        const clicked = this.style.backgroundColor;
-        
-        // Checks if the right answer was clicked
-        if(clicked === correct){
-            message.textContent = "Correct!";
-            h1.style.backgroundColor = correct;
-            reset.textContent = "Play again?";
-            colorChange(clicked);
-            console.log("You clicked on: " + clicked);
-        }else{
-            this.style.backgroundColor = "darkblue";
-            message.textContent = "Try again!";
-            console.log("You clicked on " + clicked);
-        }
-    });
-};
 
 // Changes all squares to the color of the correct square
 function colorChange(color){
@@ -124,6 +80,54 @@ function randomColor(){
     const b = Math.floor(Math.random() * 256);
 
     return "rgb(" + r + ", " + g  + ", " + b + ")";
+};
+function init(){
+    
+    picker.textContent = correct;
+    // Mode listeners
+    for(var i = 0; i < mode.length; i++){
+        mode[i].addEventListener("click", function(){
+            // If one is selected when the other is clicked, turn it off
+            // I came up with this part on my own. 
+            if(mode[0] === "selected"){
+                select();
+            }else{
+                select();
+            }
+            
+            // Complicated if statement with a ternary operator 
+            this.textContent ==="Easy" ? numsquares = 3: numsquares = 6;
+            
+            change();
+            console.log("Mode clicked. ");
+        });
+    }
+    // Fills in the squares
+    // Sets the span to show the correct variable's RBG value
+// Loops through all of the squares and gives them a different color
+for(var i = 0; i < square.length; i++){
+    
+    square[i].style.backgroundColor = colors[i];
+    
+    // Makes all of the squares clickable colors
+    square[i].addEventListener("click", function(){
+        // Tests to see if squares are clickable
+        const clicked = this.style.backgroundColor;
+        
+        // Checks if the right answer was clicked
+        if(clicked === correct){
+            message.textContent = "Correct!";
+            h1.style.backgroundColor = correct;
+            reset.textContent = "Play again?";
+            colorChange(clicked);
+            console.log("You clicked on: " + clicked);
+        }else{
+            this.style.backgroundColor = "darkblue";
+            message.textContent = "Try again!";
+            console.log("You clicked on " + clicked);
+        }
+    });
+};
 };
 
 console.log("The correct color is: " + correct);
