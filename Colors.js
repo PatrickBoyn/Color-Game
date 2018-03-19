a0fff182c1cc52d80848705de720034c774311a3
BLACKBOX\dakil <dakilangmanok@gmail.com>
2018-03-19 13:45:41 -0400

    Toggle works now, but it doesn't do what I want it to.

--- Colors.js
+++ Colors.js
@@ -12,14 +12,13 @@ const stripe = document.getElementById("stripe");
 const instructions = document.getElementById("instructions");
 const reset = document.getElementById("new");
 var mode = document.querySelectorAll(".mode");
-const selected = document.querySelector("selected")
 
 // No longer hard coded, picked by a function at the bottom of the code
 var correct = colorPicker();
 
 for(var i = 0; i < mode.length; i++){
     mode[i].addEventListener("click", function(){
-        mode[i].classList.toggle(selected);
+        this.classList.toggle("selected");
         console.log("Mode clicked. ");
     });
 }
